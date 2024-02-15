09eca7435807e6b43cc5a78eac3e1a24d2d45076
Konstantin Parashchevin <kpstsp@gmail.com>
2024-02-15 18:11:25 +0400

    Created VS version
    Added widget version with user and share functional

---
+++ widget/WatchYourBack/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WatchYourBack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.userListBox = new System.Windows.Forms.ListBox();
+            this.currentUsersLabel = new System.Windows.Forms.Label();
+            this.sharesDataGridView = new System.Windows.Forms.DataGridView();
+            this.username = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.remoteHost = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sharedFolder = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.sharesDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // userListBox
+            // 
+            this.userListBox.FormattingEnabled = true;
+            this.userListBox.ItemHeight = 16;
+            this.userListBox.Location = new System.Drawing.Point(12, 34);
+            this.userListBox.Name = "userListBox";
+            this.userListBox.Size = new System.Drawing.Size(400, 260);
+            this.userListBox.TabIndex = 0;
+            // 
+            // currentUsersLabel
+            // 
+            this.currentUsersLabel.AutoSize = true;
+            this.currentUsersLabel.Location = new System.Drawing.Point(12, 9);
+            this.currentUsersLabel.Name = "currentUsersLabel";
+            this.currentUsersLabel.Size = new System.Drawing.Size(88, 16);
+            this.currentUsersLabel.TabIndex = 1;
+            this.currentUsersLabel.Text = "Current Users";
+            // 
+            // sharesDataGridView
+            // 
+            this.sharesDataGridView.AllowUserToAddRows = false;
+            this.sharesDataGridView.AllowUserToDeleteRows = false;
+            this.sharesDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sharesDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.username,
+            this.remoteHost,
+            this.sharedFolder});
+            this.sharesDataGridView.Location = new System.Drawing.Point(15, 343);
+            this.sharesDataGridView.Name = "sharesDataGridView";
+            this.sharesDataGridView.ReadOnly = true;
+            this.sharesDataGridView.RowHeadersWidth = 51;
+            this.sharesDataGridView.RowTemplate.Height = 24;
+            this.sharesDataGridView.Size = new System.Drawing.Size(552, 272);
+            this.sharesDataGridView.TabIndex = 2;
+            // 
+            // username
+            // 
+            this.username.HeaderText = "username";
+            this.username.MinimumWidth = 6;
+            this.username.Name = "username";
+            this.username.ReadOnly = true;
+            this.username.Width = 125;
+            // 
+            // remoteHost
+            // 
+            this.remoteHost.HeaderText = "remoteHost";
+            this.remoteHost.MinimumWidth = 6;
+            this.remoteHost.Name = "remoteHost";
+            this.remoteHost.ReadOnly = true;
+            this.remoteHost.Width = 125;
+            // 
+            // sharedFolder
+            // 
+            this.sharedFolder.HeaderText = "sharedFolder";
+            this.sharedFolder.MinimumWidth = 6;
+            this.sharedFolder.Name = "sharedFolder";
+            this.sharedFolder.ReadOnly = true;
+            this.sharedFolder.Width = 125;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(925, 742);
+            this.Controls.Add(this.sharesDataGridView);
+            this.Controls.Add(this.currentUsersLabel);
+            this.Controls.Add(this.userListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sharesDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox userListBox;
+        private System.Windows.Forms.Label currentUsersLabel;
+        private System.Windows.Forms.DataGridView sharesDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn username;
+        private System.Windows.Forms.DataGridViewTextBoxColumn remoteHost;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sharedFolder;
+    }
+}
+
---
+++ widget/WatchYourBack/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WatchYourBack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            PopulateRemoteConnections();
+            RefreshUserList();
+            // Start a timer to refresh the user list every 10 seconds
+            var timer = new System.Windows.Forms.Timer();
+            timer.Tick += (s, ev) => RefreshUserList();
+            timer.Interval = 10000; // 10 seconds
+            timer.Start();
+
+            
+        }
+
+        private void RefreshUserList()
+        {
+            //var activeUsers = GetActiveUsers();
+
+            //var activeUsers = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+            List<string> userNames = new List<string>();
+            try
+            {
+                ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                {
+                    foreach (ManagementObject obj in searcher.Get())
+                    {
+                        string userName = obj["UserName"] as string;
+                        if (!string.IsNullOrEmpty(userName))
+                        {
+                            userNames.Add(userName);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("OOPs error here");
+                userNames.Add("NIHUA!");
+            }
+            DisplayActiveUsers(userNames);
+        }
+        private void DisplayActiveUsers(List<string> users)
+        {
+            userListBox.Items.Clear();
+            userListBox.Items.AddRange(users.ToArray());
+        }
+
+        private void RefreshTimer_Tick(object sender, EventArgs e)
+        {
+            // Refresh the DataGridView every tick
+            PopulateRemoteConnections();
+        }
+
+        private void PopulateRemoteConnections()
+        {
+            // Clear existing rows
+            sharesDataGridView.Rows.Clear();
+
+            try
+            {
+                var sharedFolders = GetSharedFolders();
+                // Run the "net session" command to get remote sessions
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.FileName = "cmd.exe";
+                processStartInfo.Arguments = "/c net session";
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+
+                using (Process process = Process.Start(processStartInfo))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // Parse the output and add to DataGridView
+                            if (line.StartsWith("\\"))
+                            {
+                                string[] parts = line.Split(new[] { ' ', '\\' }, StringSplitOptions.RemoveEmptyEntries);
+                                if (parts.Length >= 4)
+                                {
+                                    string remoteHost = parts[0];
+                                    string username = parts[1];
+                                    //string clientType = parts[2];
+                                    string sharedFolder = FindSharedFolder(remoteHost, sharedFolders);
+                                    //string sharedFolder = parts[3];
+
+                                    sharesDataGridView.Rows.Add(username, remoteHost, sharedFolder);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving remote connections: " + ex.Message);
+            }
+        }
+
+        private List<string> GetSharedFolders()
+        {
+            List<string> sharedFolders = new List<string>();
+
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo();
+                processStartInfo.FileName = "cmd.exe";
+                processStartInfo.Arguments = "/c net share";
+                processStartInfo.RedirectStandardOutput = true;
+                processStartInfo.UseShellExecute = false;
+                processStartInfo.CreateNoWindow = true;
+
+                using (Process process = Process.Start(processStartInfo))
+                {
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string line;
+                        while ((line = reader.ReadLine()) != null)
+                        {
+                            // Parse the output to extract shared folder names
+                            if (line.StartsWith("    "))
+                            {
+                                sharedFolders.Add(line.TrimStart().Split(' ')[0]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error retrieving shared folders: " + ex.Message);
+            }
+
+            return sharedFolders;
+        }
+
+        private string FindSharedFolder(string remoteHost, List<string> sharedFolders)
+        {
+            // Match the shared folder with the given remote host
+            foreach (var folder in sharedFolders)
+            {
+                if (remoteHost.Equals(GetRemoteHostFromPath(folder), StringComparison.OrdinalIgnoreCase))
+                {
+                    return folder;
+                }
+            }
+            return "Unknown";
+        }
+
+        private string GetRemoteHostFromPath(string path)
+        {
+            // Extracts the remote host name from a shared folder path
+            int index = path.IndexOf('\\', 2);
+            if (index != -1)
+            {
+                return path.Substring(2, index - 2);
+            }
+            return path;
+        }
+    }
+}
